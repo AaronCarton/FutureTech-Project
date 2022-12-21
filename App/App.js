25ced919556595bbecb4c506f35236aa97154002
HyperHydroX <guillaumedebackere7@gmail.com>
2022-12-21 11:23:36 +0100

    feat: working on screens

--- App/App.js
+++ App/App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+import { mainStackNavigation } from "./screens/main";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View className="flex-1 bg-white items-center justify-center">
-      <Text className="text-red-500">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={mainStackNavigation} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
---
+++ App/screens/main.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export const mainStackNavigation = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
